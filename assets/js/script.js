5342034a88a07d5b4f57303b102f8b8b23330523
Devin Gibson <dgib21@hotmail.com>
2020-09-10 20:22:08 -0500

    displaying values from local storage on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ var synonymContainerEl = document.querySelector("#synonyms-container");
 // select type of container div
 var typeOfContainerEl = document.querySelector("#type-of-container");
 
+// search history container  
+var searchHistoryEl = document.querySelector("#history-button-container")
+
 // create list to store searched words 
 var words = [];
 
@@ -37,12 +40,13 @@ var defintionFetch = function (word) {
     }).then(function (response) {
         // check that the fetch was successful
         if (response.ok) {
+            word = word.toLowerCase().trim();
             //check if word is already in the array 
             if (!words.includes(word)) {
                 //adding new searched word to the array
                 words.push(word);
                 //call add button function 
-                
+                addbutton(word, searchHistoryEl);
                 // putting user input to local storage
                 localStorage.setItem("words", JSON.stringify(words));
 
@@ -131,18 +135,44 @@ wordSearchFormEl.addEventListener("submit", wordSearch);
 
 
 
-//MY NEW CODE 
 
-var searchHistoryEl = document.querySelector("#word")
 
 
 
 
 
 //display previous searches
-var previousSearches = function () {
+var loadSearchHistory = function () {
     // set search values to local storage
     words = JSON.parse(localStorage.getItem("words"));
-    console.log(words);
+    //check if words is empty
+    if(!words)
+    {
+        //if words is empty set it to an array 
+        words = [];
+        return;
+    }
+    for(i=0; i<words.length; i++)
+    {
+        // call add button function to add searched words and buttons to the page 
+        addbutton(words[i], searchHistoryEl);
+        
+
+    } 
+    
+}
+
+var addbutton = function (word, parentContainer) {
+
+    // creating an element for button 
+    var wordButton = document.createElement("button"); 
+    //adding classes to our created element
+    wordButton.classList = "button is-text search-history";
+    //adding whatever is in the word variable as text to the wordbutton variable 
+    wordButton.textContent = word; 
+    //making it show on the page
+    parentContainer.appendChild(wordButton);
 
 }
+
+ loadSearchHistory(); 
